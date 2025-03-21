cae3fcc1a30acd21cf3936fb9967a0635603fc15
MariaCletoR <maria.cleto.rocha@gmail.com>
2025-03-21 20:10:55 +0000

    generator

---
+++ Generator/Generator.cpp
@@ -0,0 +1,259 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+
+void writeVerticesToFile(const string& filename, const vector<float>& vertices) {
+    ofstream file(filename);
+    if (!file) {
+        cerr << "Erro ao criar o ficheiro." << endl;
+        return;
+    }
+
+    file << vertices.size() / 3 << endl;
+    for (size_t i = 0; i < vertices.size(); i += 3) {
+        file << vertices[i] << " " << vertices[i + 1] << " " << vertices[i + 2] << endl;
+    }
+
+    file.close();
+    cout << "Ficheiro" << filename << "criado"<< endl;
+}
+
+void generateBox(float length, int subdivisions, const string& filename) {
+    vector<float> vertices;
+    float step = length / subdivisions;
+    float half = length / 2;
+
+    for (int i = 0; i < subdivisions; i++) {
+        for (int j = 0; j < subdivisions; j++) {
+            float x1 = -half + j * step;
+            float y1 = -half + i * step;
+            float x2 = x1 + step;
+            float y2 = y1;
+            float x3 = x1;
+            float y3 = y1 + step;
+            float x4 = x2;
+            float y4 = y3;
+
+            vertices.insert(vertices.end(), {x1, y1, half, x2, y2, half, x3, y3, half});
+            vertices.insert(vertices.end(), {x3, y3, half, x2, y2, half, x4, y4, half});
+
+            vertices.insert(vertices.end(), {x1, y1, -half, x3, y3, -half, x2, y2, -half});
+            vertices.insert(vertices.end(), {x3, y3, -half, x4, y4, -half, x2, y2, -half});
+
+            vertices.insert(vertices.end(), {-half, y1, x1, -half, y2, x2, -half, y3, x3});
+            vertices.insert(vertices.end(), {-half, y3, x3, -half, y2, x2, -half, y4, x4});
+            
+            vertices.insert(vertices.end(), {half, y1, x1, half, y3, x3, half, y2, x2});
+            vertices.insert(vertices.end(), {half, y3, x3, half, y4, x4, half, y2, x2});
+
+            vertices.insert(vertices.end(), {x1, half, y1, x3, half, y3, x2, half, y2});
+            vertices.insert(vertices.end(), {x3, half, y3, x4, half, y4, x2, half, y2});
+            
+            vertices.insert(vertices.end(), {x1, -half, y1, x2, -half, y2, x3, -half, y3});
+            vertices.insert(vertices.end(), {x3, -half, y3, x2, -half, y2, x4, -half, y4});
+        }
+    }
+
+    writeVerticesToFile(filename, vertices);
+}
+
+
+void generatePlane(float length, int subdivisions, const string& filename) {    
+
+    vector<float> vertices;
+    float step = length / subdivisions;  
+    float half = length / 2;             
+
+    for (int i = 0; i < subdivisions; i++) {
+        for (int j = 0; j < subdivisions; j++) {
+            float x1 = -half + j * step;
+            float z1 = -half + i * step;
+            float x2 = x1 + step;
+            float z2 = z1;
+            float x3 = x1;
+            float z3 = z1 + step;
+            float x4 = x2;
+            float z4 = z3;
+
+            vertices.push_back(x1); vertices.push_back(0); vertices.push_back(z1);
+            vertices.push_back(x3); vertices.push_back(0); vertices.push_back(z3);
+            vertices.push_back(x2); vertices.push_back(0); vertices.push_back(z2);
+            
+            vertices.push_back(x3); vertices.push_back(0); vertices.push_back(z3);
+            vertices.push_back(x4); vertices.push_back(0); vertices.push_back(z4);
+            vertices.push_back(x2); vertices.push_back(0); vertices.push_back(z2);
+        }
+    }
+
+    writeVerticesToFile(filename, vertices);
+}
+
+
+
+
+void generateSphere(float radius, int slices, int stacks, const string& filename) {
+    vector<float> vertices;
+    
+    for (int i = 0; i < stacks; i++) {
+        float theta1 = M_PI * i / stacks;
+        float theta2 = M_PI * (i + 1) / stacks;
+
+        for (int j = 0; j < slices; j++) {
+            float phi1 = 2 * M_PI * j / slices;
+            float phi2 = 2 * M_PI * (j + 1) / slices;
+
+            float x1 = radius * sin(theta1) * cos(phi1);
+            float y1 = radius * cos(theta1);
+            float z1 = radius * sin(theta1) * sin(phi1);
+
+            float x2 = radius * sin(theta1) * cos(phi2);
+            float y2 = radius * cos(theta1);
+            float z2 = radius * sin(theta1) * sin(phi2);
+
+            float x3 = radius * sin(theta2) * cos(phi1);
+            float y3 = radius * cos(theta2);
+            float z3 = radius * sin(theta2) * sin(phi1);
+
+            float x4 = radius * sin(theta2) * cos(phi2);
+            float y4 = radius * cos(theta2);
+            float z4 = radius * sin(theta2) * sin(phi2);
+
+            vertices.push_back(x2); vertices.push_back(y1); vertices.push_back(z2);
+            vertices.push_back(x3); vertices.push_back(y3); vertices.push_back(z3);
+            vertices.push_back(x1); vertices.push_back(y2); vertices.push_back(z1);
+
+            vertices.push_back(x2); vertices.push_back(y2); vertices.push_back(z2);
+            vertices.push_back(x4); vertices.push_back(y3); vertices.push_back(z4);
+            vertices.push_back(x3); vertices.push_back(y4); vertices.push_back(z3);
+        }
+    }
+
+    writeVerticesToFile(filename, vertices);
+}
+
+
+void generateCone(float radius, float height, int slices, int stacks, const string& filename) {
+    vector<float> vertices;
+    float angleStep = 2 * M_PI / slices;
+    float stackHeight = height / stacks;
+
+    for (int i = 0; i < slices; i++) {
+        float theta1 = i * angleStep;
+        float theta2 = (i + 1) * angleStep;
+
+        float x1 = radius * cos(theta1);
+        float z1 = radius * sin(theta1);
+        float x2 = radius * cos(theta2);
+        float z2 = radius * sin(theta2);
+
+        vertices.push_back(0);  vertices.push_back(0);  vertices.push_back(0);
+        vertices.push_back(x1); vertices.push_back(0);  vertices.push_back(z1);
+        vertices.push_back(x2); vertices.push_back(0);  vertices.push_back(z2);
+    }
+
+    for (int i = 0; i < stacks; i++) {
+        float r1 = radius * (1 - (float)i / stacks);
+        float r2 = radius * (1 - (float)(i + 1) / stacks);
+        float y1 = i * stackHeight;
+        float y2 = (i + 1) * stackHeight;
+
+        for (int j = 0; j < slices; j++) {
+            float theta1 = j * angleStep;
+            float theta2 = (j + 1) * angleStep;
+
+            float x1 = r1 * cos(theta1);
+            float z1 = r1 * sin(theta1);
+            float x2 = r1 * cos(theta2);
+            float z2 = r1 * sin(theta2);
+            float x3 = r2 * cos(theta1);
+            float z3 = r2 * sin(theta1);
+            float x4 = r2 * cos(theta2);
+            float z4 = r2 * sin(theta2);
+
+            vertices.push_back(x1); vertices.push_back(y1); vertices.push_back(z1);
+            vertices.push_back(x3); vertices.push_back(y2); vertices.push_back(z3);
+            vertices.push_back(x2); vertices.push_back(y1); vertices.push_back(z2);
+            vertices.push_back(x2); vertices.push_back(y1); vertices.push_back(z2);
+            vertices.push_back(x3); vertices.push_back(y2); vertices.push_back(z3);
+            vertices.push_back(x4); vertices.push_back(y2); vertices.push_back(z4);
+        }
+    }
+
+    float yTop = height;
+    float lastRadius = radius * (1 - (float)(stacks - 1) / stacks); 
+
+    for (int i = 0; i < slices; i++) {
+        float theta1 = i * angleStep;
+        float theta2 = (i + 1) * angleStep;
+
+        float x1 = lastRadius * cos(theta1);
+        float z1 = lastRadius * sin(theta1);
+        float x2 = lastRadius * cos(theta2);
+        float z2 = lastRadius * sin(theta2);
+
+        vertices.push_back(0);  vertices.push_back(yTop); vertices.push_back(0);
+        vertices.push_back(x2); vertices.push_back(height - stackHeight); vertices.push_back(z2);
+        vertices.push_back(x1); vertices.push_back(height - stackHeight); vertices.push_back(z1);
+    }
+
+    writeVerticesToFile(filename, vertices);
+}
+
+
+
+int main(int argc, char* argv[]) {
+    if ((argc != 5) && (argc != 6) && (argc != 7)) {
+        cerr << "Input inválido." << endl;
+        return 1;
+    }
+
+    string type = argv[1];
+
+    if (type == "plane") {
+        float length = stof(argv[2]);
+        int subdivisions = stoi(argv[3]);
+        string filename = argv[4];
+
+        generatePlane(length, subdivisions, filename);
+    }
+
+    else if (type == "box") {
+        float length = stof(argv[2]);
+        int subdivisions = stoi(argv[3]);
+        string filename = argv[4];
+
+        generateBox(length,subdivisions,filename);
+    }
+
+    else if (type == "sphere") {
+        float raio = stof(argv[2]);
+        int fatias = stoi(argv[3]);
+        int stacks = stoi(argv[4]);
+        string filename = argv[5];
+
+        generateSphere(raio,fatias,stacks,filename);
+    }
+
+    else if (type == "cone") {
+        float radius = stof(argv[2]);
+        float height = stof(argv[3]);
+        int slices = stoi(argv[4]);
+        int stacks = stoi(argv[5]);
+        string filename = argv[6];
+    
+        generateCone(radius, height, slices, stacks, filename);
+    }
+    
+    else {
+        cerr << "Figura inválida." << endl;
+    }
+
+    return 0;
+}   
---
+++ Generator/engine.cpp
@@ -0,0 +1,187 @@
+#include <GL/glut.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+using namespace std;
+
+
+std::vector<float> vertices;
+std::vector<std::string> modelFiles;
+
+
+struct Camera {
+    float posX, posY, posZ;
+    float lookAtX, lookAtY, lookAtZ;
+    float upX, upY, upZ;
+    float fov, nearc, farc;
+} camera;
+
+
+int width;
+int height;
+
+
+void loadModel(const char* filename);
+
+void parsexml(const char* file) {
+    XMLDocument doc;
+    if (doc.LoadFile(file) != XML_SUCCESS) {
+        std::cerr << "Erro ao abrir o ficheiro XML" << std::endl;
+        return;
+    }
+    XMLElement* first = doc.FirstChildElement("world");
+    if (!first) return;
+
+    XMLElement* windowElem = first->FirstChildElement("window");
+    if (windowElem) {
+        windowElem->QueryIntAttribute("width", &width);
+        windowElem->QueryIntAttribute("height", &height);
+    }
+    XMLElement* camElem = first->FirstChildElement("camera");
+    if (camElem) {
+        XMLElement* pos = camElem->FirstChildElement("position");
+        if (pos) {
+            pos->QueryFloatAttribute("x", &camera.posX);
+            pos->QueryFloatAttribute("y", &camera.posY);
+            pos->QueryFloatAttribute("z", &camera.posZ);
+        }
+        XMLElement* lookAt = camElem->FirstChildElement("lookAt");
+        if (lookAt) {
+            lookAt->QueryFloatAttribute("x", &camera.lookAtX);
+            lookAt->QueryFloatAttribute("y", &camera.lookAtY);
+            lookAt->QueryFloatAttribute("z", &camera.lookAtZ);
+        }
+        XMLElement* up = camElem->FirstChildElement("up");
+        if (up) {
+            up->QueryFloatAttribute("x", &camera.upX);
+            up->QueryFloatAttribute("y", &camera.upY);
+            up->QueryFloatAttribute("z", &camera.upZ);
+        }
+        XMLElement* proj = camElem->FirstChildElement("projection");
+        if (proj) {
+            proj->QueryFloatAttribute("fov", &camera.fov);
+            proj->QueryFloatAttribute("near", &camera.nearc);
+            proj->QueryFloatAttribute("far", &camera.farc);
+        }
+    }
+
+    XMLElement* groupElem = first->FirstChildElement("group");
+    if (groupElem) {
+        XMLElement* modelsElem = groupElem->FirstChildElement("models");
+        if (modelsElem) {
+            XMLElement* model = modelsElem->FirstChildElement("model");
+            while (model) {
+                const char* modelFile = model->Attribute("file");
+                if (modelFile) {
+                    modelFiles.push_back(modelFile);
+                    loadModel(modelFile);
+                }
+                model = model->NextSiblingElement("model");
+            }
+        }
+    }
+}
+
+void loadModel(const char* filename) {
+    ifstream file(filename);
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Erro ao abrir o arquivo: " << filename << endl;
+        return;
+    }
+
+    bool firstLine = true;
+    while (getline(file, line)) {
+        if (firstLine) {  
+            firstLine = false;
+            continue;
+        }
+        istringstream iss(line);
+        float x, y, z;
+        if (iss >> x >> y >> z) {
+            vertices.push_back(x);
+            vertices.push_back(y);
+            vertices.push_back(z);
+        }
+    }
+    file.close();
+}
+
+void drawAxes() {
+    glLineWidth(2.0f);  
+
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-50.0f, 0.0f, 0.0f);
+    glVertex3f(50.0f, 0.0f, 0.0f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -50.0f, 0.0f);
+    glVertex3f(0.0f, 50.0f, 0.0f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, -50.0f);
+    glVertex3f(0.0f, 0.0f, 50.0f);
+    glEnd();
+}
+
+void changeSize(int w, int h) {
+    if (h == 0) h = 1;
+    float ratio = w * 1.0f / h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(camera.fov, ratio, camera.nearc, camera.farc);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(camera.posX, camera.posY, camera.posZ,
+        camera.lookAtX, camera.lookAtY, camera.lookAtZ,
+        camera.upX, camera.upY, camera.upZ);
+
+    glDisable(GL_DEPTH_TEST);
+    drawAxes();
+    glEnable(GL_DEPTH_TEST);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glBegin(GL_TRIANGLES);
+    for (size_t i = 0; i < vertices.size(); i += 3) {
+        glVertex3f(vertices[i], vertices[i + 1], vertices[i + 2]);
+    }
+    glEnd();
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+    parsexml("config.xml");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("3D Engine");
+
+    glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(changeSize);
+
+    glutMainLoop();
+    return 0;
+}
